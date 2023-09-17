62cbf1e49537bc7236fc1c085ac402d097b67b50
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-17 17:27:38 +0800

    add maximum value of movements

--- script.js
+++ script.js
@@ -193,3 +193,12 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //   return acc +cur
 // }, 0)
 // console.log(balance);
+
+// Maximum value
+const max = movements.reduce((acc,mov)=>{
+  if ( acc > mov){
+    return acc
+  }else{
+    return mov
+  }
+}, movements[0])
