cd7b7eab9571fa096ece9aea87fbf1a1174820fa
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-19 15:42:26 +0800

    finish coding challenge 4

--- script.js
+++ script.js
@@ -483,40 +483,40 @@ const anyDeposits = movements.some(mov => mov > 0);
 // console.log(convertTitleCase('this is a LONG title but not too long'));
 // console.log(convertTitleCase('and here is another title with an EXAMPLE'));
 
-const dogs = [
-  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
-  { weight: 8, curFood: 200, owners: ['Matilda'] },
-  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
-  { weight: 31, curFood: 340, owners: ['Micheal'] }
-];
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 31, curFood: 340, owners: ['Micheal'] }
+// ];
 
-//1.
-dogs.forEach(dog => {
-  dog.recommendedFood = Math.trunc( dog.weight ** 0.75 * 28)
-})
+// //1.
+// dogs.forEach(dog => {
+//   dog.recommendedFood = Math.trunc( dog.weight ** 0.75 * 28)
+// })
 
-//2.
-const sarah = dogs.find(dog => dog.owners.includes('Sarah'))
-console.log(`Sarah's dog is eating ${sarah.curFood > sarah.recommendedFood ? "much": "little"}`);
+// //2.
+// const sarah = dogs.find(dog => dog.owners.includes('Sarah'))
+// console.log(`Sarah's dog is eating ${sarah.curFood > sarah.recommendedFood ? "much": "little"}`);
 
-const ownersEatTooMuch = dogs.filter(dog=> dog.curFood > dog.recommendedFood).flatMap(owner => owner.owners)
-console.log(ownersEatTooMuch);
+// const ownersEatTooMuch = dogs.filter(dog=> dog.curFood > dog.recommendedFood).flatMap(owner => owner.owners)
+// console.log(ownersEatTooMuch);
 
-const ownersEatTooLittle = dogs.filter(dogs=> dogs.curFood< dogs.recommendedFood).flatMap(owner => owner.owners)
-console.log(ownersEatTooLittle);
+// const ownersEatTooLittle = dogs.filter(dogs=> dogs.curFood< dogs.recommendedFood).flatMap(owner => owner.owners)
+// console.log(ownersEatTooLittle);
 
 
-console.log(ownersEatTooMuch.join(" and ") + "'s eat too much");
-console.log(ownersEatTooLittle.join(" and ") + "'s eat too little");
-//5
-console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
-//6
-const checkEatingOkay = dog => dog.curFood >= dog.recommendedFood * 0.9 &&  dog.recommendedFood * 1.1
-console.log(dogs.some(checkEatingOkay));
+// console.log(ownersEatTooMuch.join(" and ") + "'s eat too much");
+// console.log(ownersEatTooLittle.join(" and ") + "'s eat too little");
+// //5
+// console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+// //6
+// const checkEatingOkay = dog => dog.curFood >= dog.recommendedFood * 0.9 &&  dog.recommendedFood * 1.1
+// console.log(dogs.some(checkEatingOkay));
 
-//7
-console.log(dogs.filter(checkEatingOkay));
+// //7
+// console.log(dogs.filter(checkEatingOkay));
 
-//8
-const dogsSorted = dogs.slice().sort((a,b)=> a.recFood - b.recFood)
-console.log(dogsSorted);
+// //8
+// const dogsSorted = dogs.slice().sort((a,b)=> a.recFood - b.recFood)
+// console.log(dogsSorted);
